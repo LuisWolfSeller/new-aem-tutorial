4e51e60eaa74039d47117a75ac612423540fde18
luis carlos dominguez <luisdominguez@wolfsellers.com>
2025-10-30 17:57:06 -0600

    agrego archivos de configuracion

--- blocks/custbutton/custbutton.js
+++ blocks/custbutton/custbutton.js
@@ -7,16 +7,12 @@ export default function decorate(block) {
   // Add behavior
   button.addEventListener('click', async () => {
     console.log('aqui')
-    let res = await fetch("https://publish-p131012-e1277260.adobeaemcloud.com/graphql/execute.json/bimbodam/second-query",{
-      method: "GET",
-      credentials: "include",
-      headers: { "Accept": "application/json" }
-    })
+    let res = await fetch("https://publish-p153442-e1604645.adobeaemcloud.com/graphql/execute.json/Prueba-Modelos-Telcel/queryTestTelcel")
     let data = await res.json()
     console.log('data',data)
   });
 
-  // Clear any existing content in the block (optional)
+  // Clear any existing csimoneontent in the block (optional)
   block.textContent = '';
 
   // Append the button
